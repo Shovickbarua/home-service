6187fdc96c98bc0c0abe966f5f12ec0f2bd0c696
Shovickbarua <shovickbarua92@gmail.com>
2024-01-19 22:37:35 +0600

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './App/Screens/LoginScreen/Login';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigations from './App/Navigations/TabNavigations';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <ClerkProvider 
+    // tokenCache={tokenCache}
+    // publishableKey='pk_test_YWNjZXB0ZWQtbW9sbHVzay04Ny5jbGVyay5hY2NvdW50cy5kZXYk'>
+      <View style={styles.container}>
+        {/* Sign In */}
+        {/* <SignedIn>
+          <NavigationContainer>
+            <TabNavigations/>
+          </NavigationContainer>
+        </SignedIn> */}
+        {/* Sign Out */}
+        {/* <SignedOut> */}
+          <Login/>
+        {/* </SignedOut>
+        <StatusBar style='auto' /> */}
+      </View>
+    // </ClerkProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ App/Screens/LoginScreen/Login.jsx
@@ -0,0 +1,86 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import Colors from '../../Utils/Colors'
+import * as WebBrowser from "expo-web-browser";
+import { useWarmUpBrowser } from '../../hooks/warmUpBrowser';
+import { useOAuth } from "@clerk/clerk-expo";
+const tokenCache = {
+    async getToken(key) {
+      try {
+        return SecureStore.getItemAsync(key);
+      } catch (err) {
+        return null;
+      }
+    },
+    async saveToken(key, value) {
+      try {
+        return SecureStore.setItemAsync(key, value);
+      } catch (err) {
+        return;
+      }
+    },
+  };
+WebBrowser.maybeCompleteAuthSession()
+export default function Login() {
+    useWarmUpBrowser();
+
+    const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+    const onPress = React.useCallback(async () => {
+        try {
+          const { createdSessionId, signIn, signUp, setActive } =
+            await startOAuthFlow();
+     
+          if (createdSessionId) {
+            setActive({ session: createdSessionId });
+          } else {
+            // Use signIn or signUp for next steps such as MFA
+          }
+        } catch (err) {
+          console.error("OAuth error", err);
+        }
+      }, []);
+
+  return (
+    <View style={{alignItems:'center'}}>
+      <Image source={require('./../../../assets/images/login.png')}
+      style={styles.loginImage}
+      />
+      <View style={styles.subContainer}>
+        <Text style={{fontSize:27, color:Colors.WHITE,textAlign:'center'}}>
+            Let's Find <Text style={{fontWeight:'bold'}}>Professional Cleaning and Repair</Text> Services
+        </Text>
+        <Text style={{fontSize:17, color:Colors.WHITE,textAlign:'center',marginTop:20}}>Best App to find Services near you which deliver you a professional service</Text>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={{textAlign:'center',fontSize:17,color:Colors.PRIMARY}}>Let's get started</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    loginImage:{
+        width:230,
+        height:450,
+        marginTop:70,
+        borderWidth:4,
+        borderColor:Colors.BLACK,
+        borderRadius:15
+    },
+    subContainer:{
+        width:'100%',
+        backgroundColor:Colors.PRIMARY,
+        height:'50%',
+        marginTop:-20,
+        borderTopLeftRadius:30,
+        borderTopRightRadius:30,
+        padding:20,
+    },
+    button:{
+        padding:15,
+        backgroundColor:Colors.WHITE,
+        borderRadius:99,
+        marginTop:40,
+    }
+})
